9527a8881550eae225064a486f75392d243534e3
marcelo-martins <marcelobmartins219@hotmail.com>
2019-11-05 23:26:25 +0000

    added .s file with o3 optimization flag

--- helloworldO3.s
+++ helloworldO3.s
@@ -1,12 +1,9 @@
 	.file	"helloworld.c"
 	.abiversion 2
 	.section	".text"
-	.section	.rodata
-	.align 3
-.LC0:
-	.string	"Hello World!"
-	.section	".text"
+	.section	.text.startup,"ax",@progbits
 	.align 2
+	.p2align 4,,15
 	.globl main
 	.type	main, @function
 main:
@@ -15,23 +12,23 @@ main:
 	addi 2,2,.TOC.-.LCF0@l
 	.localentry	main,.-main
 	mflr 0
-	std 0,16(1)
-	std 31,-8(1)
-	stdu 1,-48(1)
-	mr 31,1
 	addis 3,2,.LC0@toc@ha
 	addi 3,3,.LC0@toc@l
+	std 0,16(1)
+	stdu 1,-32(1)
 	bl puts
 	nop
-	li 9,0
-	mr 3,9
-	addi 1,31,48
+	addi 1,1,32
+	li 3,0
 	ld 0,16(1)
 	mtlr 0
-	ld 31,-8(1)
 	blr
 	.long 0
-	.byte 0,0,0,1,128,1,0,1
+	.byte 0,0,0,1,128,0,0,0
 	.size	main,.-main
+	.section	.rodata.str1.8,"aMS",@progbits,1
+	.align 3
+.LC0:
+	.string	"Hello World!"
 	.ident	"GCC: (Ubuntu 7.4.0-1ubuntu1~18.04.1) 7.4.0"
 	.section	.note.GNU-stack,"",@progbits
